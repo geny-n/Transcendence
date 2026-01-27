ea5bd58f180bd3b267e8da48913e333ae4692468
ngeny <ngeny@ecole43.myguest.virtualbox.org>
2026-01-28 00:24:13 +0100

    liaison back + front _docker la connection n est pas encore faite sur front

--- backend/src/routes/server.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { serverHealth } from "../handlers/server";
-
-const router = Router();
-
-router.get('/health', serverHealth);
-router.post('/refresh');
-
-export default router;

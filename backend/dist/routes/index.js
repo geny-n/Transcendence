7c7f4fff04ced9d3a62caa09aaeb0f2030ecf406
ngeny <ninon.geny@gmail.com>
2026-01-28 17:17:19 +0100

    ignore

--- backend/dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import usersRouter from './users.js';
-import serverRouter from './server.js';
-const router = Router();
-router.use(usersRouter);
-router.use(serverRouter);
-export default router;
-//# sourceMappingURL=index.js.map
--- backend/dist/routes/server.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { serverHealth, refreshTokens } from "../handlers/server.js";
-const router = Router();
-router.get('/health', serverHealth);
-router.post('/refresh', refreshTokens);
-export default router;
-//# sourceMappingURL=server.js.map

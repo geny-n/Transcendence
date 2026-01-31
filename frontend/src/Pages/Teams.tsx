4d845f078bb8747756ce9ac40d3fa24e5c09e2d4
gtoure <japhet.romeo@gmail.com>
2026-01-31 11:08:41 +0100

    Restructurer le backend, ajouter Docker et OAuth

---
+++ frontend/src/Pages/Teams.tsx
@@ -0,0 +1,38 @@
+
+export default function Teams ()
+{
+    return (
+        <div className='text-white text-3xl
+        min-h-screen flex items-center justify-center'>
+        Teams
+    </div>
+    )
+}
+
+{/* <div>
+  <div>
+    <div class="sticky top-0 ...">A</div>
+    <div>
+      <div>
+        <img src="/img/andrew.jpg" />
+        <strong>Andrew Alfred</strong>
+      </div>
+      <div>
+        <img src="/img/aisha.jpg" />
+        <strong>Aisha Houston</strong>
+      </div>
+      <!-- ... -->
+    </div>
+  </div>
+  <div>
+    <div class="sticky top-0">B</div>
+    <div>
+      <div>
+        <img src="/img/bob.jpg" />
+        <strong>Bob Alfred</strong>
+      </div>
+      <!-- ... -->
+    </div>
+  </div>
+  <!-- ... -->
+</div> */}

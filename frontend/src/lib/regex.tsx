a5dfe1d3e80c391d20a5e4b89e843668f6ab1a22
giovany-phr <giovany.paupher@gmail.com>
2026-02-08 21:48:06 +0100

    register message fix

--- frontend/src/lib/regex.tsx
+++ frontend/src/lib/regex.tsx
@@ -1,2 +1,4 @@
 export const PasswordRegex = 
-/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/;
+/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?+_#-)=(])[A-Za-z\d@$!%*?&]/;
+
+export const ForbidenRegex = /^[^ '"`\\/<>&]+$/;

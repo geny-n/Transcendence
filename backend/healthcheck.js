0e505ed1999b009ceb8bb4bc2070b816ffe36918
Aimrad <lamalaestgangx80@gmail.com>
2026-02-17 15:29:02 +0100

    Update healthcheck.js

--- backend/healthcheck.js
+++ backend/healthcheck.js
@@ -1,10 +1,13 @@
-import { request as _request } from "http";
+import { request as _request } from "https";
+import fs from 'fs';
 
 const options = {
     timeout: 2000,
     host: "localhost",
     port: process.env.PORT || 3100,
-    path: "/health"
+    path: "/api/health",
+    key: fs.readFileSync('/etc/ssl/private/backend-selfsigned.key'),
+    cert: fs.readFileSync('/etc/ssl/certs/backend-selfsigned.crt')
 };
 
 const request = _request(options, res => {

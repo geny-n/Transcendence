869e10bea799f3016eef9bbd7d8b55a890b030bf
giovany-phr <giovany.paupher@gmail.com>
2026-03-09 23:08:32 +0100

    responsive navbar

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { NavLink } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 
 const Navbar: React.FC = () => {
   const { t, i18n } = useTranslation()
+  const [menuOpen, setMenuOpen] = useState(false)
 
   const listItems = [
     {label: t('navbar.home'), path: "/"},
@@ -21,15 +22,31 @@ const Navbar: React.FC = () => {
   return (
     <div className="nav-style">
 
-  <ul className="flex gap-8 text-xl">
-        {listItems.map(({ label, path }) => (
-            <NavLink className="relative group cursor-pointer"
-                key={label} to={path}>
-                {label}
-                <span className="btn-nav"></span>
-            </NavLink>
-        ))}
-  </ul>
+      <button className="dropdown-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </button>
+
+      {menuOpen && (
+        <ul className="dropdown-menu">
+              {listItems.map(({ label, path }) => (
+                  <NavLink className="text-white hover:bg-orange-600"
+                      onClick={() => setMenuOpen(false)}
+                      key={label} to={path}>
+                      {label}
+                  </NavLink>
+              ))}
+        </ul>
+      )}
+
+      <ul className="hidden lg:flex gap-8 text-xl whitespace-nowrap">
+            {listItems.map(({ label, path }) => (
+                <NavLink className="relative group cursor-pointer"
+                    key={label} to={path}>
+                    {label}
+                    <span className="btn-nav"></span>
+                </NavLink>
+            ))}
+      </ul>
 
   <NavLink to="/login" className="btn-login">{t('navbar.login')}</NavLink>
   <select onChange={(e) => changeLanguage(e.target.value)}
